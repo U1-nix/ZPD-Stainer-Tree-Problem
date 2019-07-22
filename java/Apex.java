e088c3324239931c1c4d722ab4d8730b0b288c4a
U1_nix <47254981+U1-nix@users.noreply.github.com>
2019-07-22 14:13:23 +0300

    apex class added

---
+++ java/Apex.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Apex {
+    private double functionValue;
+    private ArrayList coordinates;
+
+    public double getFunctionValue() {
+        return functionValue;
+    }
+
+    public void setFunctionValue(double functionValue) {
+        this.functionValue = functionValue;
+    }
+
+    public ArrayList getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(ArrayList coordinates) {
+        this.coordinates = coordinates;
+    }
+}
