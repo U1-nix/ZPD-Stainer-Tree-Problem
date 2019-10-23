23bb7247e96d7010c43a6a8594595f33e8bf7768
U1_nix <47254981+U1-nix@users.noreply.github.com>
2019-10-23 13:49:12 +0300

    - Lots of changes, but still doesn't work

---
+++ java/trees/Minimising.java
@@ -0,0 +1,41 @@
+package trees;
+
+import trees.model.TreeApex;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Minimising {
+
+    public static double minimisingFunction(List<Double> coordinates, List<TreeApex> apexes) {
+        int size = apexes.get(0).getCoordinates().size();
+        int startingValue = 0;
+        for (TreeApex e : apexes) {
+            if (e.isAdditional()) {
+                List<Double> coords = new ArrayList<>();
+                for (int i = startingValue; i < size + startingValue; i++) {
+                    coords.add(coordinates.get(i));
+                    //coordinates.remove(i);
+                }
+                e.setCoordinates(coords);
+                startingValue += size;
+            }
+        }
+        clearAllConnections(apexes);
+        Prim.createMinimumSpanningTree(apexes);
+        double sum = Tools.calculateTreeLength(apexes);
+//        System.out.println("\n" + "Coordinates: " + coordinates);
+//        System.out.println("Current tree length: " + sum + "\n");
+        return sum;
+    }
+
+    private static void clearAllConnections(List<TreeApex> apexes) {
+        for (TreeApex e : apexes) {
+            if (e.getPreviousApexId() != -2) {
+                e.setPreviousApexId(-1);
+            }
+            e.setConnectedFurther(false);
+            e.setDistanceToParent(Double.MAX_VALUE);
+        }
+    }
+}
